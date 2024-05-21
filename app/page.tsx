c6f200dc56a996042f3209add02f5a1d8f4206f5
apple <phamduythanh2004tb@gmail.com>
2024-05-21 13:32:34 +0700

    fall leaves

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,9 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div>
+    <div className="flex flex-col">
       <Link href='/glass-card'>Glass card</Link>
+      <Link href='/fall-leaves'>Fall leaves</Link>
     </div>
   );
 }
