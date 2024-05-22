261fc7203fcd28305db92580f2daccc749e0c1ff
dieu.pham <phamquangdieu97@gmail.com>
2024-05-22 13:33:17 +0700

    horizontal scroll with ReactJS

--- app/horizontal-scroll/page.tsx
+++ app/horizontal-scroll/page.tsx
@@ -1,24 +1,52 @@
-import React, { useEffect } from 'react'
+'use client';
+import React, { useEffect, useRef } from 'react'
 import styles from './styles.module.css';
+import { gsap } from "gsap";
+    
+import { useGSAP } from '@gsap/react';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+
+gsap.registerPlugin(useGSAP, ScrollTrigger);
 
 const HorizontalScroll = () => {
+    const container = useRef<HTMLDivElement>(null);
+    useGSAP(() => {
+        if (document) {
+            let sections = gsap.utils.toArray(`.${styles.panel}`);
+            console.log(sections);
+            gsap.to(sections, {
+                xPercent: -100 * (sections.length - 1),
+                ease: "none",
+                scrollTrigger: {
+                    trigger: `.${styles.container}`,
+                    pin: true,
+                    scrub: 1,
+                    snap: 1 / (sections.length - 1),
+                    end: () => "+=" + document.querySelector<HTMLElement>(`.${styles.container}`)?.offsetWidth
+                }
+            });
+        }
+    },
+    { scope: container }
+    )
+
     return (
-        <div className={styles.wrapper}>
-            <div id="test1" className={styles.container}>
-                <div className={`panel1 ${styles.description} ${styles.panel} ${styles.blue}`}>
+        <div className={styles.wrapper} ref={container}>
+            <div className={`res ${styles.container}`}>
+                <div className={`${styles.description} ${styles.panel} ${styles.blue}`}>
                     <div>
                     SCROLL DOWN
                     <div className="scroll-down"><div className="arrow"></div></div>
                     </div>
                 </div>
 
-                <section className={`panel1 ${styles.panel} ${styles.red}`}>
+                <section className={`${styles.panel} ${styles.red}`}>
                     ONE
                 </section>
-                <section className={`panel1 ${styles.panel} ${styles.orange}`}>
+                <section className={`${styles.panel} ${styles.orange}`}>
                     TWO
                 </section>
-                <section className={`panel1 ${styles.panel} ${styles.purple}`}>
+                <section className={`${styles.panel} ${styles.purple}`}>
                     THREE
                 </section>
             </div>
--- public/test.js
+++ public/test.js
@@ -1,16 +1,16 @@
-gsap.registerPlugin(ScrollTrigger);
+// gsap.registerPlugin(ScrollTrigger);
 
-let sections = gsap.utils.toArray(".panel1");
-console.log(sections);
+// let sections = gsap.utils.toArray(".panel1");
+// console.log(sections);
 
-gsap.to(sections, {
-  xPercent: -100 * (sections.length - 1),
-  ease: "none",
-  scrollTrigger: {
-    trigger: "#test1",
-    pin: true,
-    scrub: 1,
-    snap: 1 / (sections.length - 1),
-    end: () => "+=" + document.querySelector("#test1").offsetWidth
-  }
-});
+// gsap.to(sections, {
+//   xPercent: -100 * (sections.length - 1),
+//   ease: "none",
+//   scrollTrigger: {
+//     trigger: "#test1",
+//     pin: true,
+//     scrub: 1,
+//     snap: 1 / (sections.length - 1),
+//     end: () => "+=" + document.querySelector("#test1").offsetWidth
+//   }
+// });
