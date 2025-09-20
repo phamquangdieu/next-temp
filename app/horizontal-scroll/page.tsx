9adb4ebeaa3ed9aa2201d328b33d1c952e9b9996
phamquangdieu97@gmail.com <phamquangdieu97@gmail.com>
2025-09-20 20:52:54 +0700

    test convert scrollTrigger inline to create

--- app/horizontal-scroll/page.tsx
+++ app/horizontal-scroll/page.tsx
@@ -14,45 +14,53 @@ const HorizontalScroll = () => {
     () => {
       if (document) {
         let sections = gsap.utils.toArray(`.${styles.panel}`);
-        console.log(sections);
+
+        // master horizontal scroll tween
         const scrollTween = gsap.to(sections, {
-          // xPercent: -100 * (sections.length - 1),
           x: `-${4000 - window.innerWidth}px`,
           ease: 'none',
-          scrollTrigger: {
-            trigger: `.${styles.container}`,
-            pin: true,
-            scrub: 1,
-            // snap: 1 / (sections.length - 1),
-            end: () =>
-              '+=' +
-              document.querySelector<HTMLElement>(`.${styles.container}`)
-                ?.offsetWidth,
-          },
         });
-        gsap.to('.box-1', {
+
+        // ScrollTrigger controlling the horizontal scroll
+        ScrollTrigger.create({
+          trigger: `.${styles.container}`,
+          pin: true,
+          scrub: 1,
+          animation: scrollTween,
+          end: () =>
+            '+=' +
+            document.querySelector<HTMLElement>(`.${styles.container}`)
+              ?.offsetWidth,
+        });
+
+        // .box-1 animation
+        const box1Tween = gsap.to('.box-1', {
           y: -130,
           duration: 2,
           ease: 'elastic',
-          scrollTrigger: {
-            trigger: '.box-1',
-            containerAnimation: scrollTween,
-            //   start: "left center",
-            toggleActions: 'play none none reset',
-          },
         });
-        gsap.to('.box-2', {
+
+        ScrollTrigger.create({
+          trigger: '.box-1',
+          animation: box1Tween,
+          containerAnimation: scrollTween,
+          toggleActions: 'play none none reset',
+        });
+
+        // .box-2 animation
+        const box2Tween = gsap.to('.box-2', {
           y: -250,
           backgroundColor: '#1e90ff',
           ease: 'none',
-          display: 'none',
-          scrollTrigger: {
-            trigger: '.box-2',
-            containerAnimation: scrollTween,
-            start: 'center 80%',
-            end: 'center 20%',
-            scrub: true,
-          },
+        });
+
+        ScrollTrigger.create({
+          trigger: '.box-2',
+          animation: box2Tween,
+          containerAnimation: scrollTween,
+          start: 'center 80%',
+          end: 'center 20%',
+          scrub: true,
         });
       }
     },
